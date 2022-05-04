6eec63c9adc6d0c4a6eaff52c82b8da308ab281a
Shivam Kumar <sshivam962@gmail.com>
2022-05-04 10:41:15 +0530

    JWT Authorization

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :items
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  post 'authenticate', to: 'authentication#authenticate'
+
+end
